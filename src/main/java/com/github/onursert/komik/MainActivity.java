0567f0421fded398dd69ebee77a4cd936ed931ac
Onur <43381986+onursert@users.noreply.github.com>
2020-12-27 14:43:14 +0300

    Update MainActivity.java

--- src/main/java/com/github/onursert/komik/MainActivity.java
+++ src/main/java/com/github/onursert/komik/MainActivity.java
@@ -306,12 +306,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(intentContact);
                 break;
             case R.id.nav_bookpub:
-                Intent intentBookPub = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.github.onursert.bookpub"));
-                startActivity(intentBookPub);
+                launchApp("com.github.onursert.bookpub");
                 break;
             case R.id.nav_leaf:
-                Intent intentLeaf = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.github.onursert.leaf"));
-                startActivity(intentLeaf);
+                launchApp("com.github.onursert.leaf");
                 break;
         }
         drawer.closeDrawer(GravityCompat.START);
@@ -334,6 +332,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             } catch (ActivityNotFoundException ignored) {
             }
         Toast.makeText(this, "Couldn't find an email app and account", Toast.LENGTH_LONG).show();
+    }
+    private void launchApp(String packageName) {
+        Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
+        if (intent != null) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        } else {
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.parse("market://details?id=" + packageName));
+            startActivity(intent);
+        }
     }
     @Override
     public void onBackPressed() {
